36690a592230315e6ce48f1325cdc51367bb9eba
DandyForever <dandy4ever31@gmail.com>
2020-11-25 01:06:41 +0300

    Added class machine state with byte array based memory

---
+++ src/machine_state.cpp
@@ -0,0 +1,83 @@
+#include "machine_state.h"
+
+MachineState::MachineState(uint32_t initial_pc, std::vector<uint32_t>& words) : pc(initial_pc) {
+    for (size_t i = 0; i < regs_num; i++) {
+        if (i == 2)
+            regs[i] = 5000;
+        else
+            regs[i] = 0;
+    }
+    memory.resize(words.size() * sizeof(uint32_t));
+    memcpy(&(memory[0]), (void*)words.data(), words.size()*sizeof(uint32_t));
+}
+
+void MachineState::SetPC(uint32_t value) { pc = value; }
+
+void MachineState::SetReg(uint8_t num, uint32_t value) {
+    if (num >= regs_num)
+        errx(EXIT_FAILURE, "Such register (%d) doesn't exist", num);
+    else
+        regs[num] = value;
+}
+
+uint32_t MachineState::GetPC() { return pc; }
+
+uint32_t GetReg(uint8_t num) {
+    if (num >= regs_num)
+        errx(EXIT_FAILURE, "Such register (%d) doesn't exist", num);
+    return regs[num];
+}
+
+uint8_t right_shifted_byte(uint32_t data, size_t shift) { return (uint8_t) (data >> shift); }
+
+void MachineState::WriteWord(uint32_t va, uint32_t data) {
+    if (va + 3 > memory.size())
+        errx(EXIT_FAILURE, "Such address (%#010x) is OOM", va);
+
+    memory[va]     = right_shifted_byte(data, 0);
+    memory[va + 1] = right_shifted_byte(data, 8);
+    memory[va + 2] = right_shifted_byte(data, 16);
+    memory[va + 3] = right_shifted_byte(data, 24);
+}
+
+void MachineState::WriteHalfWord(uint32_t va, uint16_t data) {
+    if (va + 1 > memory.size())
+        errx(EXIT_FAILURE, "Such address (%#010x) is OOM", va);
+
+    memory[va]     = right_shifted_byte(data, 0);
+    memory[va + 1] = right_shifted_byte(data, 8);
+}
+
+void MachineState::WriteByte(uint32_t va, uint8_t data) {
+    if (va > memory.size())
+        errx(EXIT_FAILURE, "Such address (%#010x) is OOM", va);
+
+    memory[va] = right_shifted_byte(data, 0);
+}
+
+uint32_t left_shifted_word(uint8_t data, size_t shift) { return (uint32_t) data << shift; }
+
+uint32_t MachineState::ReadWord(uint32_t va) {
+    if (va + 3 > memory.size())
+        errx(EXIT_FAILURE, "Such address (%#010x) is OOM", va);
+
+    return left_shifted_word(memory[va + 3], 24) |
+           left_shifted_word(memory[va + 2], 16) |
+           left_shifted_word(memory[va + 1], 8)  |
+           left_shifted_word(memory[va], 0);
+}
+
+uint16_t MachineState::ReadHalfWord(uint32_t va) {
+    if (va + 1 > memory.size())
+        errx(EXIT_FAILURE, "Such address (%#010x) is OOM", va);
+
+    return (uint16_t) (left_shifted_word(memory[va + 1], 8) |
+                       left_shifted_word(memory[va], 0));
+}
+
+uint8_t MachineState::ReadByte(uint32_t va) {
+    if (va > memory.size())
+        errx(EXIT_FAILURE, "Such address (%#010x) is OOM", va);
+    
+    return memory[va];
+}
--- src/machine_state.h
+++ src/machine_state.h
@@ -1,7 +1,11 @@
+
+
 class MachineState{
 public:
-    MachineState();
-    ~MachineState();
+    const int regs_num = 32;
+
+    MachineState(uint32_t initial_pc, std::vector<uint32_t>& words);
+    ~MachineState() {}
 
     void SetPC(uint32_t value);
     void SetReg(uint8_t num, uint32_t value);
@@ -10,12 +14,17 @@ public:
     uint32_t GetReg(uint8_t num);
 
     void WriteWord(uint32_t va, uint32_t data);
+    void WriteHalfWord(uint32_t va, uint16_t data);
+    void WriteByte(uint32_t va, uint8_t data);
     uint32_t ReadWord(uint32_t va);
+    uint16_t ReadHalfWord(uint32_t va);
+    uint8_t ReadByte(uint32_t va);
 
-    uint32_t Fetch(uint32_t va);
+    //uint32_t Fetch(uint32_t va);
 
 private:
     uint32_t pc;
-    uint32_t reg[33];
-    MMU mmu;
+    uint32_t regs[32];
+    //MMU mmu;
+    std::vector<uint8_t> memory;
 };
