2e9e25a8f1fbf50b37f8763475f87d103d9b1f9a
Daniil Vorobyev <vorobyev.da@phystech.edu>
2020-12-05 22:46:52 +0300

    Exceptions are added.

--- src/exception.h
+++ src/exception.h
@@ -1,11 +1,13 @@
 #pragma once
+
 #include <exception>
 
-class MachineException: public std::exception {
+class MachineException : public std::exception {
 public:
-    MachineException (const char* msg) : message(msg) {}
-    const char* what() { return message; }
+    MachineException(const char *msg) : message(msg) {}
+
+    const char *what() { return message; }
 
 private:
-    const char* message;
+    const char *message;
 };
